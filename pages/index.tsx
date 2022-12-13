7e5f48700adffa13b79297237b7786b27d3b45ad
MaxHiit <pro.maxcode@gmail.com>
2022-12-13 08:08:43 +0100

    feat(components): refacto global components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,63 @@
-import WeatherLeft from '../src/components/WeatherLeft';
-import SearchIcon from '../src/components/SearchIcon';
-import { useEffect, useState } from 'react';
-import WeatherDetailItem from '../src/components/WeatherDetailItem';
-import ImageBgComponent from '../src/components/ImageBgComponent';
-import getWeather from './services/weather.service';
+import { useState } from 'react';
 import { Weather } from '../types';
+import { getWeather } from '@/services/weather.service';
+import WeatherLeft from '@/components/Weather';
+import WeatherDetailItem from '@/components/WeatherDetailItem';
+import ImageBgComponent from '@/components/ImageBgComponent';
+import MessageWidget from '@/components/MessageWidget';
+import SearchBar from '@/components/Search';
+import FindeMe from '@/components/FindeMe';
 
-type Props = {
-	weatherData: Weather;
-};
+const Home: React.FC = () => {
+	const [weather, setWeather] = useState<Weather>({
+		id: 0,
+		city: '',
+		temp: 0,
+		icon: '',
+		main: '',
+		timezone: 0,
+		stats: []
+	});
+	const [errorMessage, setErrorMessage] = useState('');
 
-const Home: React.FC<Props> = ({ weatherData }) => {
-	const [city, setCity] = useState<string | null>(null);
-	const [weather, setWeather] = useState<Weather>(weatherData);
+	const { city, temp, icon, main, stats, timezone } = weather;
 
-	const { cityName, temp, icon, main, stats, dt, timezone } = weather;
-
-	const handleOnSubmit = async (e: React.SyntheticEvent) => {
+	const handleOnSubmit = async (e: React.SyntheticEvent, searchValue: string) => {
 		e.preventDefault();
-		const data = await getWeather(city as string);
+		if (!searchValue) return;
+		const data = await getWeather(searchValue as string);
 		setWeather(data);
-	};
-
-	const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-		setCity(e.target.value);
 	};
 
 	return (
 		<>
 			<ImageBgComponent main={main} />
 			<div className='flex'>
-				<WeatherLeft city={cityName} temp={temp} icon={icon} main={main} dt={dt} timezone={timezone} />
-				<div className='glass pl-10 pb-10 w-full max-w-[500px] flex flex-col justify-between'>
-					<form className='flex justify-between gap-20' onSubmit={handleOnSubmit}>
-						<input
-							type='text'
-							name='location'
-							id='loaction'
-							className='h-20 px-3 w-full bg-transparent border-b-2 border-white text-white placeholder:text-white'
-							placeholder='Type a location'
-							onChange={handleInputOnChange}
-						/>
-						<button type='submit' className='h-20 p-10 flex items-center justify-center bg-[#84a69e]'>
-							<SearchIcon />
-						</button>
-					</form>
-					<div className='border-t-2 border-b-2 border-white py-5 mt-10 mr-10'>
-						{stats?.map((item) => (
-							<WeatherDetailItem key={item.id} name={item.name} value={item.value} />
-						))}
+				<div className='flex-1 p-20 h-screen flex flex-col justify-between'>
+					<h1 className='text-2xl font-bold'>the.weather</h1>
+					{weather.city ? (
+						<WeatherLeft city={city} temp={temp} icon={icon} main={main} timezone={timezone} />
+					) : (
+						<MessageWidget message='No data' />
+					)}
+				</div>
+				<div className='glass pl-10 pb-10 w-full max-w-[500px] flex flex-col'>
+					<SearchBar onSearchSubmit={handleOnSubmit} />
+					<div className='mt-5'>
+						<FindeMe setErrorChange={setErrorMessage} setWeatherChange={setWeather} />
+						{errorMessage && <MessageWidget message={errorMessage} />}
+					</div>
+					<div className='border-t-2 border-b-2 border-white py-5 mt-auto mr-10'>
+						{weather.stats?.length ? (
+							stats?.map((item) => <WeatherDetailItem key={item.id} name={item.name} value={item.value} />)
+						) : (
+							<MessageWidget message='No data' />
+						)}
 					</div>
 				</div>
 			</div>
 		</>
 	);
-};
-export const getServerSideProps = async () => {
-	const data = await getWeather();
-
-	return {
-		props: {
-			weatherData: data
-		}
-	};
 };
 
 export default Home;
